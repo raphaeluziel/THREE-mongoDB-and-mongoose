fc82601e9b4527fe8186a734f22ee663229242b0
Glitch (hospitable-alibi) <none>
2019-01-04 21:26:23 +0000

    🕴👸 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -40,7 +40,24 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 // <Your code here >
 
-var Person /* = <Your Model> */
+/*
+const Schema = mongoose.Schema;
+var personSchema = new Schema({
+  name : {type: String, required: true},
+  age : {type: Number},
+  favoriteFoods : [String]
+})
+var Person = mongoose.model('Person', personSchema);
+*/
+
+var Schema = mongoose.Schema;
+var person = new Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods: [String]
+});
+
+var Person = mongoose.model('Person', person);
 
 // **Note**: GoMix is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
